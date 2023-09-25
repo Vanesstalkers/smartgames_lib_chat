bd94323581a554f42d0c7de556ae3a561a05c9c9
VanesStalkers <ivan.mironov@bk.ru>
2023-09-25 13:48:52 +0300

    * await broadcaster.publishAction

--- api/update.js
+++ api/update.js
@@ -2,7 +2,7 @@ async (context, { text, channel }) => {
   const { sessionId } = context.session.state;
   const session = lib.store('session').get(sessionId);
   const user = session.user();
-  lib.store.broadcaster.publishAction(channel, 'updateChat', {
+  await lib.store.broadcaster.publishAction(channel, 'updateChat', {
     text,
     // сохраняем user.name, потому что после перезапуска сервера в store может не быть нужного user
     user: { id: user.id(), name: user.name },
