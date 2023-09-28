0632f9d9aacaf646b9da614dd602038a7e0e5ef7
VanesStalkers <ivan.mironov@bk.ru>
2023-09-28 14:52:20 +0300

    * рефакторинг

--- api/openPersonal.js
+++ api/openPersonal.js
@@ -1,21 +1,17 @@
 async (context, { id: channelUserId, name }) => {
-  try {
-    const { sessionId } = context.session.state;
-    const session = lib.store('session').get(sessionId);
-    const user = session.user();
-    const userId = user.id();
+  const { sessionId } = context.session.state;
+  const session = lib.store('session').get(sessionId);
+  const user = session.user();
+  const userId = user.id();
 
-    if (!user.name) throw new Error('Для открытия личного чата должно быть указано имя пользователя');
+  if (!user.name) throw new Error('Для открытия личного чата должно быть указано имя пользователя');
 
-    user.set({ personalChatMap: { [channelUserId]: { name } } });
-    await user.saveChanges();
+  user.set({ personalChatMap: { [channelUserId]: { name } } });
+  await user.saveChanges();
 
-    await lib.store.broadcaster.publishData(`user-${channelUserId}`, { personalChatMap: { [userId]: { name: user.name } } });
+  await lib.store.broadcaster.publishData(`user-${channelUserId}`, {
+    personalChatMap: { [userId]: { name: user.name } },
+  });
 
-    return { status: 'ok' };
-  } catch (err) {
-    console.log(err);
-    context.client.emit('session/error', { message: err.message, stack: err.stack });
-    return err;
-  }
+  return { status: 'ok' };
 };
